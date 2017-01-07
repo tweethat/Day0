b62aa949cba32f8d83b7a442c3101f47773ab692
tweethat <tweethat@yahoo.com>
2017-01-07 13:08:59 +0100

    modify aritGeo library.js

--- aritGeo/app/library.js
+++ aritGeo/app/library.js
@@ -3,32 +3,30 @@
 module.exports = {
  
  aritGeo: function(inputArray) {
- 	var ap, gp, exp, diff, n = inputArray.length;
- 	if (inputArray.length == 0) {
- 		return 0;
- 	}
- 	else {
- 		ap = inputArray[1] - inputArray[0];
- 		gp = inputArray[1] / inputArray[0];
+   var ap, gp,exp,diff, n = inputArray.length ;
+   if (inputArray.length == 0) {
+   	return 0;
+   }
+   else {
+   	ap = inputArray[1] - inputArray[0];
+   	gp = inputArray[1] / inputArray[0];
 
- 		for (var ind = 3; ind <= n; ind++) {  //start checking for arithmetic and geometric progression from index 3
- 			exp = inputArray[ind] / inputArray[ind - 1];
+   	for (var ind = 3; ind <= n; ind++) {  //start checking for arithmetic and geometric progression from index 3
+   		exp = inputArray[ind] / inputArray[ind - 1];
 
- 			diff = inputArray[ind] - inputArray[ind - 1];
+   		diff = inputArray[ind] - inputArray[ind - 1];
 
- 			if (exp == gp) {
- 				return 'Geometric';
- 			}
- 			else if (diff == ap) {
- 				return 'Arithmetic';
- 			}
- 			else {
- 				return -1;
- 			}
-
- 		}
-
-
- }	
+   		if (exp == gp) {
+   			return 'Geometric';
+   			}
+   		else if (diff == ap) {
+   			return 'Arithmetic';
+   		 		}
+   		else {
+   			return -1;
+   		}
+ 
+   }
+}
 }
 }
