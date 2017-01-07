a16d0fcbbfa7316d0185e396d046ad09b53d45d4
tweethat <tweethat@yahoo.com>
2017-01-07 15:03:03 +0100

    update fizzbuzz library.js

--- fizzBuzz/app/library.js
+++ fizzBuzz/app/library.js
@@ -4,14 +4,14 @@ module.exports = {
  
  fizzBuzz: function(num) {
    //var min, max;
-   if (num % 3 == 0) {
-   	return "Fizz";
+   if ((num % 3 == 0) && (num % 5 == 0)) {
+   	return "FizzBuzz";
    }
    else if (num % 5 == 0) {
    	return "Buzz";
    }
-   else if ((num % 3 == 0) && (num % 5 == 0)) {
-   	return "fizzBuzz";
+   else if (num % 3 == 0) {
+   	return "Fizz";
    }
    else {
    	return num;
